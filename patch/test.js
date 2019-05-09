52d22a794639fed9cbf182c1340186decf02c2ce
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-05-08 23:57:22 -0400

    more objects

--- patch/test.js
+++ patch/test.js
@@ -3,8 +3,8 @@ module.exports = {
     description: 'tester description',
     flags: '-t, --test',
     patch: [
-       // { offset: 0x60D5, value: "0B" },
-       // { offset: 0x60D9, value: "07" },
-        { offset: 0x60D9, value: "47" }
+        //{ offset: 0x60D5, value: "1C" },
+        { offset: 0x60D9, value: "1E" },
+        { offset: 0x60DD, value: "2D" }
     ]
 };
