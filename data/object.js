d81d510a12e9c6003098556b39a6f307896cf1b9
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-05-20 17:52:53 -0400

    remapping enemy functions

--- data/object.js
+++ data/object.js
@@ -1,3 +1,5 @@
+const camelcase = require('camelcase');
+
 const ORB = 0x25;
 const SACRED_FLAME = 0x26;
 const BOOK = 0x27;
@@ -50,6 +52,11 @@ const CAMILLA = 0x42;
 const DEATH = 0x42;
 const DRACULA = 0x42;
 
+const POS_AIR = exports.POS_AIR = 1;
+const POS_GROUND = exports.POS_GROUND = 2;
+const POS_BELOW = exports.POS_BELOW = 3;
+const POS_DEADHAND = exports.POS_DEADHAND = 4;
+
 // Map object ids to their sprite pattern table index. The pattern table index is the 
 // value used to get from this array. For example, if you wanted an array of all the 
 // objects ids that use the mansion sprite pattern table (0x09), you'd access the array 
@@ -84,8 +91,10 @@ const npc = exports.npc = function() {
 };
 
 const enemy = exports.enemy = function() {
+  const opts = arguments[arguments.length - 1];
   const o = obj.apply(null, arguments);
   o.enemy = true;
+  if (opts.pos) { o.pos = opts.pos; }
   return o;
 };
 
@@ -150,138 +159,183 @@ npc.shepherd = function() {
 }
 
 // define shorthand functions for all enemies
-enemy.bat = function() {
-  return enemy.call(null, ...arguments, BAT, 'bat');
-};
+const enemies = [
+  { name: 'bat', id: BAT, pos: POS_AIR, sprite: 0x01 },
+  { name: 'blob', id: BLOB, pos: POS_GROUND, sprite: 0x05 },
+  { name: 'bone dragon', id: BONE_DRAGON, pos: POS_GROUND, sprite: 0x05 },
+  { name: 'bone thrower', id: BONE_THROWER, pos: POS_GROUND, sprite: 0x09 },
+  { name: 'dead hand', id: DEAD_HAND, pos: POS_DEADHAND, sprite: 0x05 },
+  { name: 'eagle', id: EAGLE, pos: POS_AIR, sprite: 0x07 },
+  { name: 'eyeball', id: EYEBALL, pos: POS_AIR, sprite: 0x03 },
+  { name: 'fire ghoul', id: FIRE_GHOUL, pos: POS_GROUND, sprite: 0x05 },
+  { name: 'fishman', id: FISHMAN, pos: POS_BELOW, sprite: 0x03 },
+  { name: 'floating skull', id: FLOATING_SKULL, pos: POS_AIR, sprite: 0x05 },
+  { name: 'flower', id: FLOWER, pos: POS_GROUND, sprite: 0x07 },
+  { name: 'gargoyle', id: GARGOYLE, pos: POS_GROUND, sprite: 0x09 },
+  { name: 'ghost', id: GHOST, pos: POS_AIR, sprite: 0x07 },
+  { name: 'grabber', id: GRABBER, pos: POS_GROUND, sprite: 0x05 },
+  { name: 'harpy', id: HARPY, pos: POS_AIR, sprite: 0x07 },
+  { name: 'high jump leech', id: HIGH_JUMP_LEECH, pos: POS_BELOW, sprite: 0x05 },
+  { name: 'leech', id: LEECH, pos: POS_BELOW, sprite: 0x03 },
+  { name: 'lizardman', id: LIZARDMAN, pos: POS_GROUND, sprite: 0x03 },
+  { name: 'mansion bat', id: MANSION_BAT, pos: POS_AIR, sprite: 0x09 },
+  { name: 'mansion blob', id: MANSION_BLOB, pos: POS_GROUND, sprite: 0x09 },
+  { name: 'medusa', id: MEDUSA, pos: POS_AIR, sprite: 0x07 },
+  { name: 'mudman', id: MUDMAN, pos: POS_GROUND, sprite: 0x03 },
+  { name: 'mummy', id: MUMMY, pos: POS_GROUND, sprite: 0x07 },
+  { name: 'rock', id: ROCK, pos: POS_AIR, sprite: 0x09 },
+  { name: 'skeleton', id: SKELETON, pos: POS_GROUND, sprite: [ 0x03, 0x05, 0x09 ] },
+  { name: 'spear knight', id: SPEAR_KNIGHT, pos: POS_GROUND, sprite: 0x09 },
+  { name: 'spider', id: SPIDER, pos: POS_AIR, sprite: [ 0x03, 0x09 ] },
+  { name: 'swamp ghoul', id: SWAMP_GHOUL, pos: POS_GROUND, sprite: 0x05 },
+  { name: 'werewolf', id: WEREWOLF, pos: POS_GROUND, sprite: 0x03 },
+  { name: 'wolf', id: WOLF, pos: POS_GROUND, sprite: 0x03 },
+  { name: 'zigzag bat', id: ZIGZAG_BAT, pos: POS_AIR, sprite: 0x03 },
+  { name: 'zombie', id: ZOMBIE, pos: POS_GROUND, sprite: 0x01 }
+];
 
-enemy.harpy = function() {
-  return enemy.call(null, ...arguments, HARPY, 'harpy');
-};
+enemies.forEach(({ name, id, pos }) => {
+  const key = camelcase(name);
+  enemy[id] = enemy[key] = function(x, y, data, pointer, opts = {}) {
+    return Object.assign({ name, id, pos, x, y, data, pointer, enemy: true }, opts);
+  };
+  enemy[id].id = enemy[key].id = id;
+  enemy[id].pos = enemy[key].pos = pos;
+  enemy[id].name = enemy[key].name = name;
+});
 
-enemy.medusa = function() {
-  return enemy.call(null, ...arguments, MEDUSA, 'medusa');
-};
+// enemy.bat = function() {
+//   return enemy.call(null, ...arguments, BAT, 'bat', { pos: POS_AIR });
+// };
 
-enemy.flower = function() {
-  return enemy.call(null, ...arguments, FLOWER, 'flower');
-};
+// enemy.harpy = function() {
+//   return enemy.call(null, ...arguments, HARPY, 'harpy', { pos: POS_AIR });
+// };
 
-enemy.ghost = function() {
-  return enemy.call(null, ...arguments, GHOST, 'ghost');
-};
+// enemy.medusa = function() {
+//   return enemy.call(null, ...arguments, MEDUSA, 'medusa', { pos: POS_AIR });
+// };
 
-enemy.eagle = function() {
-  return enemy.call(null, ...arguments, EAGLE, 'eagle');
-};
+// enemy.flower = function() {
+//   return enemy.call(null, ...arguments, FLOWER, 'flower', { pos: POS_GROUND });
+// };
 
-enemy.mummy = function() {
-  return enemy.call(null, ...arguments, MUMMY, 'mummy');
-};
+// enemy.ghost = function() {
+//   return enemy.call(null, ...arguments, GHOST, 'ghost', { pos: POS_AIR });
+// };
 
-enemy.mansionBat = function() {
-  return enemy.call(null, ...arguments, MANSION_BAT, 'mansion bat');
-};
+// enemy.eagle = function() {
+//   return enemy.call(null, ...arguments, EAGLE, 'eagle', { pos: POS_AIR });
+// };
 
-enemy.blob = function() {
-  return enemy.call(null, ...arguments, BLOB, 'blob');
-};
+// enemy.mummy = function() {
+//   return enemy.call(null, ...arguments, MUMMY, 'mummy', { pos: POS_GROUND });
+// };
 
-enemy.mansionBlob = function() {
-  return enemy.call(null, ...arguments, MANSION_BLOB, 'mansion blob');
-};
+// enemy.mansionBat = function() {
+//   return enemy.call(null, ...arguments, MANSION_BAT, 'mansion bat', { pos: POS_AIR });
+// };
 
-enemy.boneDragon = function() {
-  return enemy.call(null, ...arguments, BONE_DRAGON, 'bone dragon');
-};
+// enemy.blob = function() {
+//   return enemy.call(null, ...arguments, BLOB, 'blob', { pos: POS_GROUND });
+// };
 
-enemy.boneThrower = function() {
-  return enemy.call(null, ...arguments, BONE_THROWER, 'bone thrower');
-};
+// enemy.mansionBlob = function() {
+//   return enemy.call(null, ...arguments, MANSION_BLOB, 'mansion blob', { pos: POS_GROUND });
+// };
+
+// enemy.boneDragon = function() {
+//   return enemy.call(null, ...arguments, BONE_DRAGON, 'bone dragon', { pos: POS_GROUND });
+// };
+
+// enemy.boneThrower = function() {
+//   return enemy.call(null, ...arguments, BONE_THROWER, 'bone thrower', { pos: POS_GROUND });
+// };
 
 enemy.camilla = function() {
-  return enemy.call(null, ...arguments, CAMILLA, 'camilla');
+  return enemy.call(null, ...arguments, CAMILLA, 'camilla', { pos: POS_AIR });
 };
 
-enemy.deadHand = function() {
-  return enemy.call(null, ...arguments, DEAD_HAND, 'dead hand');
-};
+// enemy.deadHand = function() {
+//   return enemy.call(null, ...arguments, DEAD_HAND, 'dead hand', { pos: POS_DEADHAND });
+// };
 
 enemy.death = function() {
-  return enemy.call(null, ...arguments, DEATH, 'death');
+  return enemy.call(null, ...arguments, DEATH, 'death', { pos: POS_AIR });
 };
 
-enemy.eyeball = function() {
-  return enemy.call(null, ...arguments, EYEBALL, 'eyeball');
-};
+// enemy.eyeball = function() {
+//   return enemy.call(null, ...arguments, EYEBALL, 'eyeball', { pos: POS_AIR });
+// };
 
-enemy.fireGhoul = function() {
-  return enemy.call(null, ...arguments, FIRE_GHOUL, 'fire ghoul');
-};
+// enemy.fireGhoul = function() {
+//   return enemy.call(null, ...arguments, FIRE_GHOUL, 'fire ghoul', { pos: POS_GROUND });
+// };
 
-enemy.fishman = function() {
-  return enemy.call(null, ...arguments, FISHMAN, 'fishman');
-};
+// enemy.fishman = function() {
+//   return enemy.call(null, ...arguments, FISHMAN, 'fishman', { pos: POS_BELOW });
+// };
 
-enemy.floatingSkull = function() {
-  return enemy.call(null, ...arguments, FLOATING_SKULL, 'floating skull');
-};
+// enemy.floatingSkull = function() {
+//   return enemy.call(null, ...arguments, FLOATING_SKULL, 'floating skull', { pos: POS_AIR });
+// };
 
-enemy.gargoyle = function() {
-  return enemy.call(null, ...arguments, GARGOYLE, 'gargoyle');
-};
+// enemy.gargoyle = function() {
+//   return enemy.call(null, ...arguments, GARGOYLE, 'gargoyle', { pos: POS_GROUND });
+// };
 
-enemy.grabber = function() {
-  return enemy.call(null, ...arguments, GRABBER, 'grabber');
-};
+// enemy.grabber = function() {
+//   return enemy.call(null, ...arguments, GRABBER, 'grabber', { pos: POS_GROUND });
+// };
 
-enemy.highJumpLeech = function() {
-  return enemy.call(null, ...arguments, HIGH_JUMP_LEECH, 'high jump leech');
-};
+// enemy.highJumpLeech = function() {
+//   return enemy.call(null, ...arguments, HIGH_JUMP_LEECH, 'high jump leech', { pos: POS_BELOW });
+// };
 
-enemy.leech = function() {
-  return enemy.call(null, ...arguments, LEECH, 'leech');
-};
+// enemy.leech = function() {
+//   return enemy.call(null, ...arguments, LEECH, 'leech', { pos: POS_BELOW });
+// };
 
-enemy.lizardman = function() {
-  return enemy.call(null, ...arguments, LIZARDMAN, 'lizardman');
-};
+// enemy.lizardman = function() {
+//   return enemy.call(null, ...arguments, LIZARDMAN, 'lizardman', { pos: POS_GROUND });
+// };
 
-enemy.mudman = function() {
-  return enemy.call(null, ...arguments, MUDMAN, 'mudman');
-};
+// enemy.mudman = function() {
+//   return enemy.call(null, ...arguments, MUDMAN, 'mudman', { pos: POS_GROUND });
+// };
 
-enemy.rock = function() {
-  return enemy.call(null, ...arguments, ROCK, 'rock');
-};
+// enemy.rock = function() {
+//   return enemy.call(null, ...arguments, ROCK, 'rock', { pos: POS_AIR });
+// };
 
-enemy.skeleton = function() {
-  return enemy.call(null, ...arguments, SKELETON, 'skeleton');
-};
+// enemy.skeleton = function() {
+//   return enemy.call(null, ...arguments, SKELETON, 'skeleton', { pos: POS_GROUND });
+// };
 
-enemy.spearKnight = function() {
-  return enemy.call(null, ...arguments, SPEAR_KNIGHT, 'spear knight');
-};
+// enemy.spearKnight = function() {
+//   return enemy.call(null, ...arguments, SPEAR_KNIGHT, 'spear knight', { pos: POS_GROUND });
+// };
 
-enemy.spider = function() {
-  return enemy.call(null, ...arguments, SPIDER, 'spider');
-};
+// enemy.spider = function() {
+//   return enemy.call(null, ...arguments, SPIDER, 'spider', { pos: POS_AIR });
+// };
 
-enemy.swampGhoul = function() {
-  return enemy.call(null, ...arguments, SWAMP_GHOUL, 'swamp ghoul');
-};
+// enemy.swampGhoul = function() {
+//   return enemy.call(null, ...arguments, SWAMP_GHOUL, 'swamp ghoul', { pos: POS_GROUND });
+// };
 
-enemy.werewolf = function() {
-  return enemy.call(null, ...arguments, WEREWOLF, 'werewolf');
-};
+// enemy.werewolf = function() {
+//   return enemy.call(null, ...arguments, WEREWOLF, 'werewolf', { pos: POS_GROUND });
+// };
 
-enemy.wolf = function() {
-  return enemy.call(null, ...arguments, WOLF, 'wolf');
-};
+// enemy.wolf = function() {
+//   return enemy.call(null, ...arguments, WOLF, 'wolf', { pos: POS_GROUND });
+// };
 
-enemy.zigzagBat = function() {
-  return enemy.call(null, ...arguments, ZIGZAG_BAT, 'zigzag bat');
-};
+// enemy.zigzagBat = function() {
+//   return enemy.call(null, ...arguments, ZIGZAG_BAT, 'zigzag bat', { pos: POS_AIR });
+// };
 
-enemy.zombie = function() {
-  return enemy.call(null, ...arguments, ZOMBIE, 'zombie');
-};
+// enemy.zombie = function() {
+//   return enemy.call(null, ...arguments, ZOMBIE, 'zombie', { pos: POS_GROUND });
+// };
