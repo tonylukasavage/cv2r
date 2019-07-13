5496c507ca770b83e2117728677d541bb772aa61
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-07-13 15:28:46 -0400

    rando for all laurel and oak merchants now

--- lib/itemizer/asm/saleId.asm
+++ lib/itemizer/asm/saleId.asm
@@ -25,11 +25,68 @@ LDA #$<%= garlicAlba %>
 G_DONE JMP $9999
 
 ; laurel
-L_TOP NOP
-; L_TOP_CMP_#$0F
-; BNE_DONE
+L_TOP CMP #$00
+BNE O_TOP
+LDA *$50
+
+; laurel_aljiba
+CMP #$0E
+BNE L_ALBA
+LDA #$<%= laurelsAljiba %>
+JMP $9999
+
+; laurel_alba
+L_ALBA CMP #$10
+BNE L_ONDO
+LDA #$<%= laurelsAlba %>
+JMP $9999
+
+; laurel_ondol
+L_ONDO CMP #$13
+BNE L_DOIN
+LDA #$<%= laurelsOndol %>
+JMP $9999
+
+; laurel_doina
+L_DOIN CMP #$15
+BNE L_DONE
+LDA #$<%= laurelsDoina %>
+L_DONE JMP $9999
 
 ; oak
+O_TOP CMP #$12
+BNE O_DONE
+LDA *$50
+
+; oak_ring
+CMP #$06
+BNE O_RIB
+LDA #$<%= oakRing %>
+JMP $9999
+
+; oak_rib
+O_RIB CMP #$07
+BNE O_HEAR
+LDA #$<%= oakRib %>
+JMP $9999
+
+; oak_heart
+O_HEAR CMP #$08
+BNE O_EYE
+LDA #$<%= oakHeart %>
+JMP $9999
+
+; oak_eye
+O_EYE CMP #$09
+BNE O_NAIL
+LDA #$<%= oakEye %>
+JMP $9999
+
+; oak_nail
+O_NAIL CMP #$0A
+BNE O_DONE
+LDA #$<%= oakNail %>
+O_DONE JMP $9999
 
 PLP
 RTS
