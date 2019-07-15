fc56e267179cc3843f39e4b54d1a18e0f3fc23ac
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-07-15 09:42:28 -0400

    broken text

---
+++ lib/itemizer/asm/textUnshare.asm
@@ -0,0 +1,75 @@
+PHP
+
+LDA *$7F
+
+CMP #$2F
+BNE L_TOP
+LDA *$50
+CMP #$0C
+BNE G_ALBA
+<%= garlicAljiba %>
+JMP $9999
+G_ALBA <%= garlicAlba %>
+JMP $9999
+
+L_TOP LDA *$7F
+CMP #$30
+BNE O_TOP
+LDA *$50
+
+CMP #$0E
+BNE L_ALBA
+<%= laurelsAljiba %>
+JMP $9999
+
+L_ALBA CMP #$10
+BNE L_ONDO
+<%= laurelsAlba %>
+JMP $9999
+
+L_ONDO CMP #$13
+BNE L_DOIN
+<%= laurelsOndol %>
+JMP $9999
+
+L_DOIN CMP #$15
+BNE L_NONE
+<%= laurelsDoina %>
+L_NONE JMP $9999
+
+O_TOP LDA *$7F
+CMP #$1D
+BEQ DO_OAK
+JMP $9999
+DO_OAK LDA *$50
+
+CMP #$07
+BNE O_HEAR
+<%= oakRib %>
+JMP $9999
+
+O_HEAR CMP #$08
+BNE O_EYE
+<%= oakHeart %>
+JMP $9999
+
+O_EYE CMP #$09
+BNE O_NAIL
+<%= oakEye %>
+JMP $9999
+
+O_NAIL CMP #$0A
+BNE O_RING
+<%= oakNail %>
+JMP $9999
+
+O_RING CMP #$06
+BNE F_DONE
+<%= oakRing %>
+
+F_DONE PLP
+
+LDA (*$00)
+CMP #$FF
+
+RTS
