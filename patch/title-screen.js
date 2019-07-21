f74df743c7d05df06982e3b77fe091069badfbd1
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-07-21 17:18:18 -0400

    textToHexString shorthand

--- patch/title-screen.js
+++ patch/title-screen.js
@@ -5,6 +5,6 @@ module.exports = {
 	description: 'Change text on title screen',
 	flags: '-c, --title-screen',
 	patch: [
-		{ offset: 0x01041E, value: textToHexString('BSAC RULES!!!') }
+		{ offset: 0x01041E, value: 'C1 C1 ' + textToHexString('randomizer') + ' C1 C1' }
 	]
 };
