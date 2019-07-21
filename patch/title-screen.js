8662841f8077fd625129854a59f6198c48b1e039
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-07-21 17:09:19 -0400

    text hex shorthand and patch update

--- patch/title-screen.js
+++ patch/title-screen.js
@@ -1,12 +1,10 @@
-const { textToBytes } = require('../lib/utils');
-const teststring = textToBytes("test");
+const { textToHexString } = require('../lib/utils');
 
 module.exports = {
 	name: 'Title screen',
 	description: 'Change text on title screen',
 	flags: '-c, --title-screen',
 	patch: [
-		{ offset: 0x01041E, value: "C1 C1 12 01 0E 04 0F 0D 09 1A 05 12 C1 C1" },
-		{ offset: 0x01041E, value: ""+teststring }
+		{ offset: 0x01041E, value: textToHexString('BSAC RULES!!!') }
 	]
 };
