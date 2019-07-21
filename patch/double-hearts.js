b78f56e241e1641735657f22ad84561ddc1b85f4
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-07-21 16:32:40 -0400

    handle explicit flags for --all, disable default double hearts

--- patch/double-hearts.js
+++ patch/double-hearts.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'Double Hearts',
 	description: 'double the value of each heart',
 	flags: '-d, --double-hearts',
+	explicit: true,
 	patch: [
 		{ offset: 0x474F, value: "04" },
 		{ offset: 0x475C, value: "08" },
