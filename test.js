a9adacc3566eba5e637173171a9b0e7480265da9
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-08-14 21:53:08 -0400

    make tests work on windows (with cmder.exe)
    still won't work with default command line prompt

--- test.js
+++ test.js
@@ -5,13 +5,18 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
  
 function randomString() {
-	return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+	return Math
+		.random()
+		.toString(36)
+		.substring(2, 15) + Math.random().toString(36)
+		.substring(2, 15);
 }
 
 for (let i = 0; i < 1000; i++) {
 	const seed = randomString();
 	const filename = path.join(__dirname, 'tmp', `cv2-test-${seed}`);
-	const cmd = `./bin/cv2rando --seed ${seed} --output "${filename}" cv2.nes`;
+	const bin = process.platform === 'win32' ? 'node .\\bin\\cv2r' : './bin/cv2r';
+	const cmd = `${bin} --seed ${seed} --output "${filename}" cv2.nes`;
 	test(cmd, async t => {
 		await exec(cmd);
 		t.pass();
