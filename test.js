fe917c19230526a598b86679841a9b6d537498ad
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-08-15 22:00:41 -0400

    setup travis ci

--- test.js
+++ test.js
@@ -16,7 +16,13 @@ for (let i = 0; i < 1000; i++) {
 	const seed = randomString();
 	const filename = path.join(__dirname, 'tmp', `cv2-test-${seed}`);
 	const bin = process.platform === 'win32' ? 'node .\\bin\\cv2r' : './bin/cv2r';
-	const cmd = `${bin} --seed ${seed} --output "${filename}" cv2.nes`;
+
+	// can't run ROM tests on travis 
+	let cmd = `${bin} --seed ${seed} --output "${filename}" cv2.nes`;
+	if (process.env.TRAVIS) {
+		cmd = `${bin} --seed ${seed} --json`;
+	}
+	
 	test(cmd, async t => {
 		await exec(cmd);
 		t.pass();
