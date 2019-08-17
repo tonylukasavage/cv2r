327736a125abc9ab257f32ae1e98bf3e695cb800
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-08-17 02:02:13 -0400

    crystal sale icons update progressively, fixes #14

---
+++ lib/patch/itemizer/asm/merchantIcon.asm
@@ -0,0 +1,29 @@
+LDA $<%= saleIcons %>,Y
+PHP
+CMP #$5E
+BEQ CRYS
+CMP #$5F
+BEQ CRYS
+CMP #$6E
+BNE DONE
+
+CRYS LDA *$91
+AND #$60
+CLC
+ROR A
+ROR A
+ROR A
+ROR A
+ROR A
+
+STX $6000
+TAX
+CPX #$03
+BNE LOAD
+LDA #$02
+TAX
+LOAD LDA $<%= crystalIcons %>,X
+LDX $6000
+
+DONE PLP
+RTS 
