ea6de97bfb173f33b306ac64ea8684727c862162
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-09-02 00:04:33 -0400

    diff patch generation

--- tools/diff-patch.js
+++ tools/diff-patch.js
@@ -1,14 +1,26 @@
+const _ = require('lodash');
 const fs = require('fs').promises;
+const path = require('path');
+const { patchManager: pm, utils: { validateRom } } = require('../lib');
 
-(async function main() {
-	try {
-		const [,,baseRom,hackRom] = process.argv;
+const template = `
+module.exports = {
+	id: '<%= id %>',
+	name: '<%= name %>',
+	description: '<%= description %>',
+	patch: <%= patch %>
+};
+`;
+
+const patchFuncs = {
+	diff: async function(args, opts) {
+		const [ baseRom, hackRom ] = args;
+		validateRom(baseRom);
+		validateRom(hackRom);
 		const baseBytes = await fs.readFile(baseRom);
 		const hackBytes = await fs.readFile(hackRom);
-		if (baseBytes.length !== hackBytes.length) {
-			throw new Error(`roms be same length, base rom has ${baseBytes.length}, hacked rom has ${hackBytes.length}`);
-		}
-
+		
+		// Cycle through each rom, byte by byte, creating a diff between the two
 		let patch;
 		const patches = [];
 		for (let i = 0; i < baseBytes.length; i++) {
@@ -29,9 +41,52 @@ const fs = require('fs').promises;
 			}
 		}
 
-		console.log(patches);
+		// write collected patches to module format in designated location
+		const { id, output } = opts;
+		opts.patch = JSON.stringify(patches, null, '\t')
+			.replace(/\n/g, '\n\t')
+			.replace(/"(bytes|offset)"/g, '$1');
+		const rendered = _.template(template)(opts).trim();
+		fs.writeFile(path.join(output, `${id}.js`), rendered);
+	},
+	empty: function(args, opts) {
+		return;
+	},
+	ips: function(args, opts) {
+		return;
+	}
+}
+const patchFuncList = Object.keys(patchFuncs).join(', ');
+
+module.exports = async function diff(name, opts = {}) {
+	try {
+		let { description = '', id, method = 'diff', output, type = 'optional' } = opts;
+		if (!output) {
+			if (!pm.order.includes(type)) {
+				throw new Error(`Invalid patch type "${type}", must be: ${pm.order.join(', ')}`);
+			}
+			output = path.join(__dirname, '..', 'patch', type);
+		}
+
+		// validate name and necessary options
+		if (!name) { throw new Error('must provide a name for the patch'); }
+		if (!id) { 
+			id = name.toLowerCase().replace(/\s+/g, '-').replace(/[^a-z0-9-]/g, ''); 
+		}
+		if (!/[a-z0-9-]+/.test(id)) {
+			throw new Error(`invalid id ${id}, must contain only lowercase letters, numbers, or dashes (-)`);
+		}
+		if (!method) { throw new Error(`must provide --m,method for patching, options include: ${patchFuncList}`); }
+		if (!Object.keys(patchFuncs).includes(method)) {
+			throw new Error(`invalid method ${method}, must be: ${patchFuncList}`);
+		}
+
+		// get patch fata amd compose all info gathered into a patch in js module format
+		const pfOpts = { description, id, name, output };
+		await patchFuncs[method](opts.args, pfOpts);
+
 	} catch (err) {
 		console.error(err);
 		process.exit(1);
 	}
-})();
+};
