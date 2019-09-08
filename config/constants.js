1bf3bdfb771cbc4eacc57019fdae4124f0931c8a
Tony Lukasavage <anthony.lukasavage@gmail.com>
2019-09-08 10:34:30 -0400

    fixed patch subcommand

--- config/constants.js
+++ config/constants.js
@@ -2,11 +2,12 @@ const patchTemplate = `
 module.exports = {
 	id: '<%= id %>',
 	name: '<%= name %>',
-	description: '<%= description %>',
+	description: '<%= desc %>',
 	patch: <%= patch %>
 };
 `;
 
 module.exports = {
-	PATCH_TEMPLATE: patchTemplate
+	PATCH_TEMPLATE: patchTemplate,
+	SPRITE_TABLE_OFFSET: 0x20010
 };
