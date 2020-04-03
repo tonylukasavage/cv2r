08459f34069cd0ce31e3b2f62a6a9d61fd48d670
BloodSweatAndCode <anthony.lukasavage@gmail.com>
2020-04-03 08:48:06 -0400

    initial POC of town door to mansion then back to town

---
+++ patch/optional/door-rando/index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { bank, utils: { modSubroutine } } = require('../../../lib');
+
+module.exports = {
+	pre: true,
+	id: 'door-rando',
+	name: 'Door Rando',
+	description: 'All town and mansion doors are randomized',
+	patch: function(pm) {
+		modSubroutine(pm.name, path.join(__dirname, 'town-door-enter.asm'), bank[3], {
+			invoke: {
+				romLoc: 0xC81F
+			}
+		});
+
+		modSubroutine(pm.name, path.join(__dirname, 'mansion-exit.asm'), bank[7], {
+			invoke: {
+				romLoc: 0x1D0F9
+			}
+		});
+	}
+};
---
+++ patch/optional/door-rando/town-door-enter.asm
@@ -0,0 +1,6 @@
+LDA *$30
+STA $6100
+LDA #$01
+STA *$30
+LDA #$06
+RTS
