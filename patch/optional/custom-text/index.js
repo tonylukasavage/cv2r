3f66ada25d3f0e8d3ac6830939b2543da344bdfe
BloodSweatAndCode <anthony.lukasavage@gmail.com>
2020-04-15 20:25:32 -0400

    wip

--- patch/optional/custom-text/index.js
+++ patch/optional/custom-text/index.js
@@ -1,7 +1,6 @@
-const path = require('path');
 const tp = require('./text-pointers');
-const { romToRam } = require('./helpers');
-const { bank, core, utils: { modSubroutine } } = require('../../../lib');
+const { prepText, romToRam } = require('./helpers');
+const { bank, core, utils: { modText } } = require('../../../lib');
 
 module.exports = {
 	pre: true,
@@ -13,19 +12,23 @@ module.exports = {
 		const pointers = await tp.init();
 
 		// initialize new text pointer table
-		const tableBytes = new Array(tp.pointers.length * 2);
-		tableBytes.fill(0xFF, 0, tableBytes.length);
+		// const tableBytes = new Array(tp.pointers.length * 2);
+		// tableBytes.fill(0xFF, 0, tableBytes.length);
+
+		// write new text table
+		const pointerMod = modText(pm.name, prepText('Remember when JC had the world record?'), bank[3]);
 
 		// update text pointers for all actors
 		core.forEach(loc => {
 			if (!loc.actors) { return; }
 			loc.actors.forEach(actor => {
 				if (!actor.textPointer) { return; }
+				if (actor.id !== 0xB5) { return; }
 				const pointerIndex = pointers.findIndex(p => p === romToRam(actor.textPointer));
-				console.log(pointerIndex);
+				pm.add([pointerMod.ram & 0xFF], 0xCB92 + (pointerIndex * 2));
+				pm.add([pointerMod.ram >>> 8], 0xCB92 + (pointerIndex * 2) + 1);
 
-				// actor.textOffset = (actor.textPointer - 0xCB92) / 2;
-				// console.log(actor.textOffset     );
+				// console.log(`[${(0xCB92 + (pointerIndex * 2)).toString(16)}] ${(pointerMod.ram >>> 8).toString(16)}${(pointerMod.ram & 0xFF).toString(16).padStart(2, '0')}`);
 			});
 		});
 
