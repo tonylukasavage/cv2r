ce620b6f564663b54bacb6e3023bffbc01e4ba57
Tony Lukasavage <alukasavage@innovu.com>
2020-04-19 07:32:39 -0400

    Update index.js

--- patch/optional/custom-text/index.js
+++ patch/optional/custom-text/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs').promises;
 const path = require('path');
 const { prepText, romToRam } = require('./helpers');
-const { bank, core, utils: { modText } } = require('../../../lib');
+const { bank, core, utils: { modText, randomInt } } = require('../../../lib');
 
 const BASE_ADDR_ROM = 0xCB92; // 0x8B82 RAM
 
@@ -9,7 +9,7 @@ module.exports = {
 	id: 'custom-text',
 	name: 'Custom Text',
 	description: 'All the dialog is now a little more fun in CV2R',
-	patch: async function(pm) {
+	patch: async function(pm, { rng }) {
 		// load original text pointers
 		const textPointers = [];
 		let tpData = await fs.readFile(path.join(__dirname, 'data', 'text-pointers.raw'), 'utf8');
@@ -28,8 +28,10 @@ module.exports = {
 			const file = path.join(__dirname, 'data', f);
 			const name = f.replace(/\.txt$/, '');
 			const data = await fs.readFile(file, 'utf8');
-			newText[name] = data.split('\n').map(l => l.trim()).filter(l => l).map(l => ({ rawText: l }));
+			newText[name] = data.split('\n').map(l => l.trim()).filter(l => l);
 		}
+
+		console.log(newText);
 
 		// write new text table
 		// Object.keys(newText).forEach(key => {
@@ -40,43 +42,40 @@ module.exports = {
 		// });
 		// console.log(JSON.stringify(newText, null, 2));
 
-		const pointerMod = modText(pm.name, prepText('Remember when JC had the world record?'), bank[3]);
+		// const pointerMod = modText(pm.name, prepText('Remember when JC had the world record?'), bank[3]);
 
 		// update text pointers for all actors
 		const actorMap = {
 			'Camilla': 'camilla',
 			'Death': 'death',
-			'orb': 'orb'
+			'orb': 'orb',
+			'sacred flame': 'flame',
+			'crystal dude': 'dude',
+			'man': 'npc',
+			'old lady': 'npc',
+			'woman': 'npc',
+			'shepherd': 'npc',
+			'merchant': 'merchant',
+			'secret merchant': 'garlic'
 		};
 
 		core.forEach(loc => {
 			if (!loc.actors) { return; }
 			loc.actors.forEach(actor => {
 				if (!actor.textPointer) { return; }
-				if (actor.name === 'orb') {
 
-				} else if (actor.name === 'Camilla') {
-
-				} else if (actor.name === 'Death') {
-
-				} else if (actor.name === 'sacred flame') {
-
-				} else if (actor.name === 'crystal dude') {
-
-				} else if (['man', 'old lady', 'woman', 'sheperd'].includes(actor.name)) {
-
-				} else if (actor.name === 'merchant') {
-
-				} else if (actor.name === 'secret merchant') {
-
+				const textKey = actorMap[actor.name];
+				if (!textKey) {
+					console.error('skipping ' + actor.name);
+					return;
 				}
+				const index = randomInt(rng, 0, newText[textKey].length - 1);
+				const text = newText[textKey][index].text;
+				const mod = modText(pm.name, prepText(text), bank[3]);
 
-				if (actor.id !== 0xB5) { return; }
 				const pointerIndex = textPointers.findIndex(p => p === romToRam(actor.textPointer));
-				pm.add([pointerMod.ram & 0xFF], BASE_ADDR_ROM + (pointerIndex * 2));
-				pm.add([pointerMod.ram >>> 8], BASE_ADDR_ROM + (pointerIndex * 2) + 1);
-
-				// console.log(`[${(0xCB92 + (pointerIndex * 2)).toString(16)}] ${(pointerMod.ram >>> 8).toString(16)}${(pointerMod.ram & 0xFF).toString(16).padStart(2, '0')}`);
+				pm.add([mod.ram & 0xFF], BASE_ADDR_ROM + (pointerIndex * 2));
+				pm.add([mod.ram >>> 8], BASE_ADDR_ROM + (pointerIndex * 2) + 1);
 			});
 		});
 
