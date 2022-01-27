b66ab37d341ab00c671eda6ec63ce7d2f1341044
BloodSweatAndCode <anthony.lukasavage@gmail.com>
2022-01-27 12:08:54 -0500

    move fangs code to bank 0, save 40 bytes on bank 7

--- patch/optional/fangs/index.js
+++ patch/optional/fangs/index.js
@@ -6,13 +6,13 @@ module.exports = {
 	name: 'Dracula\'s Fangs',
 	description: 'Adds Dracula\'s Fangs as a collectable item. Instantly gives you 5 lives. After every continue you will also start with 5 lives.',
 	patch: function (pm) {
-		modSubroutine(pm.name, path.join(__dirname, 'fangs.asm'), bank[7], {
+		modSubroutine(pm.name, path.join(__dirname, 'fangs.asm'), bank[0], {
 			invoke: {
 				romLoc: 0x1F1A4,
 				padding: 2
 			}
 		});
-		modSubroutine(pm.name, path.join(__dirname, 'continue.asm'), bank[7], {
+		modSubroutine(pm.name, path.join(__dirname, 'continue.asm'), bank[0], {
 			invoke: {
 				romLoc: 0x1C320,
 				padding: 1
